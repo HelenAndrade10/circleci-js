1d741f39495ef4f0f998322f2b833385bb6e3d89
Helen Andrade <helenoliveira110@gmail.com>
2023-03-22 23:52:37 -0300

    Adds test to make sure 5 + 5 is equal to 10

--- main.teste.js
+++ main.teste.js
@@ -3,5 +3,6 @@ const { Add } = require('./main');
 describe('Add function', () => {
   it('should return the sum of two numbers', () => {
     expect(Add(2, 3)).toBe(5);
+    expect(Add(5, 5)).toBe(10);
   });
 });
